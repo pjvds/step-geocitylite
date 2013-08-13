b2599337c2f8309a0f7b37dffa6a7961bfda64e0
Alan Harper <alan@aussiegeek.net>
2013-08-13 10:49:08 +1000

    Fix wrong filename being copied

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+set -e
+set -x
 
 if [ ! -f $WERCKER_CACHE_DIR/GeoLiteCity.dat ]; then
   cd /tmp
   curl -O http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
   gunzip GeoLiteCity.dat.gz
-  cp GeoLiteCity.dat.gz $WERCKER_CACHE_DIR
+  cp GeoLiteCity.dat $WERCKER_CACHE_DIR
 fi
 sudo mkdir -p /usr/share/GeoIP
 sudo cp $WERCKER_CACHE_DIR/GeoLiteCity.dat /usr/share/GeoIP
