0b9cd196b02945fb57ad2b4975fd0779efc99b6b
Pieter Joost van de Sande <pj@born2code.net>
2013-08-14 15:10:13 +0200

    Fix typo's

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 if [ ! -f $WERCKER_CACHE_DIR/GeoLiteCity.dat ]; then
   debug 'GeoLiteCity.dat not found in cache directory, will download it'
 
@@ -7,13 +6,13 @@ if [ ! -f $WERCKER_CACHE_DIR/GeoLiteCity.dat ]; then
   curl -O http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz || fail 'unable to download GeoLiteCitry.data.gz'
   gunzip GeoLiteCity.dat.gz
   cp GeoLiteCity.dat $WERCKER_CACHE_DIR
-  
+
   debug 'GeoLiteCity.dat added to cache successfully'
 else
-  debug 'GeoLiteCity.data found in cache directory'
+  debug 'GeoLiteCity.dat found in cache directory'
 fi
 
 sudo mkdir -p /usr/share/GeoIP
 sudo cp $WERCKER_CACHE_DIR/GeoLiteCity.dat /usr/share/GeoIP
 
-success 'GeoLiteCtiry.dat installed into /usr/share/GeoIP'
+success 'GeoLiteCity.dat installed into /usr/share/GeoIP'
