164b975bb2239bdc40c6a3ec732fdb942239eb9f
pjvds <pj@born2code.net>
2013-08-14 13:55:02 +0200

    Add some extra logging and make the rm comment silent

--- run.sh
+++ run.sh
@@ -3,13 +3,16 @@ if [ ! -f $WERCKER_CACHE_DIR/GeoLiteCity.dat ]; then
   debug 'GeoLiteCity.dat not found in cache directory, will download it'
 
   cd /tmp
-  rm GeoLiteCity* || debug 'no GeoLiteCitry found'
+  rm -f GeoLiteCity*
   curl -O http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz || fail 'unable to download GeoLiteCitry.data.gz'
   gunzip GeoLiteCity.dat.gz
   cp GeoLiteCity.dat $WERCKER_CACHE_DIR
   
   debug 'GeoLiteCity.dat added to cache successfully'
+else
+  debug 'GeoLiteCity.data found in cache directory'
 fi
+
 sudo mkdir -p /usr/share/GeoIP
 sudo cp $WERCKER_CACHE_DIR/GeoLiteCity.dat /usr/share/GeoIP
 
